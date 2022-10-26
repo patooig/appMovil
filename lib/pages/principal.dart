847e0e7a28179a9e2b50c49119b9c6d5f143395c
Patricio Inostroza <patooig@gmail.com>
2022-10-26 20:18:55 -0300

    Add files

---
+++ lib/pages/principal.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../global.dart';
+import 'package:demo_login/pages/login.dart';
+
+class Principal extends StatefulWidget {
+  const Principal({super.key});
+
+  @override
+  State<Principal> createState() => _PrincipalState();
+}
+
+class _PrincipalState extends State<Principal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Image.asset('usuario.png', fit: BoxFit.contain),
+            ),
+            ListTile(
+              title: const Text('Agregar'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Integrantes'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Salir'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const login()));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
